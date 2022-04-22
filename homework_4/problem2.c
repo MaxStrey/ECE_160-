dd0bd9b8fa90c8b41453a757840a7009359725d4
MaxStrey <maximilian.strey@cooper.edu>
2022-04-22 16:28:15 -0400

    uploading sos

--- homework_4/problem2.c
+++ homework_4/problem2.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main()
+int strend(char *s, char *t)
 {
-    char *str = "worldhelloworld";
-    char *nstr = "world";
-    int n = strlen(str);
-    int m = strlen(nstr);
+    int n = strlen(s);
+    int m = strlen(t);
     printf("%d\n", n);
     printf("%d\n", m);
     int counter = 0;
-    for (int i = 0; i < n; i++)
-    {       
-        for (int j = 0; j < m; j++)
+    int v = n - m;
+    int true_v = v;
+    int i = 0;
+    while (v < n)
+    {
+        printf("%c\n", *(s + v));
+        printf("  %c\n", *(t + i));
+        printf("     %d\n", i);
+        if (*(s + v) == *(t + i))
         {
-            if (j == i)
-            {
-                counter++;
-                if (counter == m)
-                {
-                    printf("nstr is at the end of str\n");
-                }
-            } 
+            counter++;
         }
+        i++;
+        v++;
     }
+    if (counter == true_v)
+    {
+        return 1;
+    }
+}
+
+
+int main()
+{
+    char *s = "helloworld";
+    char *t = "world";
+    if (strend("Helloworld", "world") == 1)
+    {
+        printf("Hooray");
+    }
+
     return 0;
 }
+
