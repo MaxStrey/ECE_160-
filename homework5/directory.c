b18ed90a7a921c0fd3a4f39c011467666a6db8ce
MaxStrey <maximilian.strey@cooper.edu>
2022-05-05 04:18:29 -0400

    updating my homework

--- homework5/directory.c
+++ homework5/directory.c
@@ -9,6 +9,8 @@ struct Filename
 };
 int find_empty(struct Filename *);
 int find_file(struct Filename *, char *);
+int make_permissions(int i, char *);
+char *output_permissions(struct Filename *, int i);
 
 int main()
 {
@@ -33,18 +35,15 @@ int main()
             while(getchar() != '\n');
             strcpy(directory[find_empty(directory)].name, dname);
             directory[find_empty(directory)].permissions = 7;
-            for (int j = 0; j < 20; j++)
-            {
-                printf("%s\n", directory[j].name);
-                printf("%d\n", directory[j].permissions);
-            }
-
         }
         else if (strcmp(input, "ls") == 0)
         {
-            for (int j = 0; j < 20; j++)
+            for (int i = 0; i < 20; i++)
             {
-                printf("%s\n", directory[j].name);
+                if (strcmp(directory[i].name, "-") != 0)
+                {
+                    printf("%s\t%s\n", output_permissions(directory, i), directory[i].name);
+                }
             }
         }
         else if (strcmp(input, "mv") == 0)
@@ -57,21 +56,19 @@ int main()
             printf("Enter name of renamed file: ");
             scanf("%256s", rename);
             while(getchar() != '\n');
-            strcpy(directory[find_file(directory, fname)-1].name, rename);
-            for (int j = 0; j < 20; j++)
-            {
-                printf("%s\n", directory[j].name);
-                printf("%d\n", directory[j].permissions);
-            }
+            strcpy(directory[find_file(directory, fname)].name, rename);
         }
         else if (strcmp(input, "chmod") == 0)
         {
             char fname[256];
+            char nper[256];
             printf("Enter name of the file you want to change permissions: ");
             scanf("%256s", fname);
             while(getchar() != '\n');
-            directory[find_file(directory, fname)-1].permissions = -1;
-            strcpy(directory[find_file(directory, fname)-1].name, "-"); 
+            printf("Enter the changed permission: ");
+            scanf("%256s", nper);
+            while(getchar() != '\n');
+            directory[find_file(directory, fname)].permissions = make_permissions(find_file(directory, fname), nper);
         }
         else if (strcmp(input, "rm") == 0)
         {
@@ -79,15 +76,15 @@ int main()
             printf("Enter name of the file you want to remove: ");
             scanf("%256s", fname);
             while(getchar() != '\n');
-            directory[find_file(directory, fname)-1].permissions = -1;
-            strcpy(directory[find_file(directory, fname)-1].name, "-"); 
-            for (int j = 0; j < 20; j++)
-            {
-                printf("%s\n", directory[j].name);
-                printf("%d\n", directory[j].permissions);
-            }
+            directory[find_file(directory, fname)].permissions = -1;
+            strcpy(directory[find_file(directory, fname)].name, "-"); 
+        }
+        else if (strcmp(input, "quit") == 0)
+        {
+            break;
         }  
     }
+    return 0;
 }
 
 int find_empty(struct Filename *directory)
@@ -108,58 +105,83 @@ int find_file(struct Filename *directory, char *fname)
 {
     for (int i = 0; i < 20; i++)
     {
-        if (strcmp(directory[i].name, fname))
+        if (strcmp(directory[i].name, fname) == 0)
         {
             return i;
         }
     }
-    return -1;
+    return 0;
 }
 
-char make_permissions(int i, struct Filename *directory, char *per)
+int make_permissions(int i, char *nper)
 {
-    char *per;
+    if (strcmp(nper, "x") == 0)
+    {
+        return 1;
+    }
+    if (strcmp(nper, "w") == 0)
+    {
+        return 2;
+    }
+    if (strcmp(nper, "wx") == 0)
+    {
+        return 3;
+    }
+    if (strcmp(nper, "r") == 0)
+    {
+        return 4;
+    }
+    if (strcmp(nper, "rx") == 0)
+    {
+        return 5;
+    }
+    if (strcmp(nper, "rw") == 0)
+    {
+        return 6;
+    }
+    if (strcmp(nper, "rwx") == 0)
+    {
+        return 7;
+    }
+    return 0;
+}
+
+char *output_permissions(struct Filename *directory, int i)
+{
     if (directory[i].permissions == 1)
     {
-        char *outputr = "x";
-        return outputr;
+        char *output = "x";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 2)
     {
-        char *outputr = "w";
-        return outputr;
+        char *output = "w";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 3)
     {
-        char *outputr = "wx";
-        return outputr;
+        char *output = "wx";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 4)
     {
-        char *outputr = "r";
-        return outputr;
+        char *output = "r";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 5)
     {
-        char *outputr = "rx";
-        return outputr;
+        char *output = "rx";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 6)
     {
-        char *outputr = "rw";
-        return outputr;
+        char *output = "rw";
+        return output;
     }
-    char *per;
     if (directory[i].permissions == 7)
     {
-        char *outputr = "rwx";
-        return outputr;
+        char *output = "rwx";
+        return output;
     }
+    return 0;
 }
-
-int find_permissions()
